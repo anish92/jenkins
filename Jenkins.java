228cab611cac3d0db7aec8749336ded5c7877fbb
Anish Puthuparambil <anish_abraham007@yahoo.com>
2018-11-19 23:10:06 -0800

    test1

--- Jenkins.java
+++ Jenkins.java
@@ -2,10 +2,6 @@ public class Jenkins
 {
 	public static void main( String[] args)
 		{
-			System.out.println("This is a Jenkins integration sample program");
+			System.out.println("This is a Jenkins integration sample program for git hub");
 		}
 }
-
-
-
-
