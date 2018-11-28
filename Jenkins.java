d57e606b8463d030e0105d545f8e5c4e1405ce73
Anish Puthuparambil <anish_abraham007@yahoo.com>
2018-11-27 22:40:14 -0800

    trigger 14

--- Jenkins.java
+++ Jenkins.java
@@ -5,7 +5,8 @@ public class Jenkins
 			System.out.println("This is a Jenkins integration sample program for github\n");
 			System.out.println("Testing auto trigger\n");
 			System.out.println("Testing auto trigger 12\n");
-			System.out.println("Testing auto trigger 12\n");
-			
+			System.out.println("Testing auto trigger 13\n");
+			System.out.println("Testing auto trigger 14\n");
+
 		}
 }
